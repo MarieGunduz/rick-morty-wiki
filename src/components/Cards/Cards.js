dfc5a30953e82fea9ea18e21d7cd0e14a65970ee
Marie Gunduz <mariegunduz@gmail.com>
2024-03-05 00:47:06 +0300

    worked on Cards.js

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,16 +1,21 @@
 import React from 'react'
 
-const Cards = ({results}) => {
- 
+const Cards = ({ results }) => {
+
   let display;
   console.log(results);
 
-  if(results) {
-    display = results.map(x => {
-      let {id, name} = x
+  if (results) {
+    display = results.map(result => {
+      let { id, name, image } = result
       return <div key={id} className="col-4">
-        {name}
-        </div>;
+        <div className="">
+          <img src={image} alt={name} className="img-fluid" />
+          <div className="content">
+            <div className="fs-4 fw-bold mb-4">{name}</div>
+          </div>
+        </div>
+      </div>
     });
   } else {
     display = "No Characters Found :/";
