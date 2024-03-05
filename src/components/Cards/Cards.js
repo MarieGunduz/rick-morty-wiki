39c1faf4e9cdebdd8967b4381cbd362ff9a5b9f2
Marie Gunduz <mariegunduz@gmail.com>
2024-03-06 00:38:18 +0300

    worked on cards and cards module

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from "react";
+import styles from "./Cards.module.scss";
 
 const Cards = ({ results }) => {
 
@@ -7,26 +8,30 @@ const Cards = ({ results }) => {
 
   if (results) {
     display = results.map(result => {
-      let { id, name, image } = result
-      return <div key={id} className="col-4">
+      let { id, name, image, location, status } = result
+      return <div key={id} className="col-4 position-relative">
         <div className="">
           <img src={image} alt={name} className="img-fluid" />
           <div className="content">
             <div className="fs-4 fw-bold mb-4">{name}</div>
             <div className="">
               <div className="fs-6">Last location</div>
+              <div className="fs-5">{location.name}</div>
             </div>
           </div>
         </div>
-      </div>
+        <div className={`${`styles.badge`} position-absolute badge bg-danger`}>
+          {status}
+          </div>
+      </div >
     });
   } else {
-    display = "No Characters Found :/";
-  }
+  display = "No Characters Found :/";
+}
 
-  return (
-    <>{display}</>
-  );
+return (
+  <>{display}</>
+);
 };
 
 export default Cards; 
