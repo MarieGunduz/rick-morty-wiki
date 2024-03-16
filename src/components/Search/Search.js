1b6bf47627d735c3385acb55b5b4411214128e8e
Marie Gunduz <mariegunduz@gmail.com>
2024-03-16 19:46:36 +0300

    add: changed app.js, worked on search function and started on pagination function

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,11 +1,19 @@
 import React from "react";
 import styles from "./Search.module.scss";
 
-const Search = ({ setSearch }) => {
+const Search = ({ setSearch, setPageNumber}) => {
     return (
         <form className="d-flex justify-content-center gap-4 mb-5">
-            <input placeholder="Search for Characters" type="text" className={styles.input} />
-            <button className={`${styles.btn} btn btn-primary fs-5`}>Search</button>
+            <input 
+            onChange={(e) => {
+                setPageNumber(1);
+                setSearch(e.target.value);
+            }}
+            placeholder="Search for Characters" type="text" className={styles.input} />
+            <button onClick={(e) => {
+                e.preventDefault();
+                }}
+                 className={`${styles.btn} btn btn-primary fs-5`}>Search</button>
         </form>
     );
 };
