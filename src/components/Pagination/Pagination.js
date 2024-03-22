bee3fb5ea5c1425f062cbfb125091a13c869674c
Marie Gunduz <mariegunduz@gmail.com>
2024-03-22 08:58:22 +0300

    add: modified pagination

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -1,19 +1,9 @@
-import React from 'react'
-import ReactPaginate from 'react-paginate';
+import React from "react";
+import ReactPaginate from "react-paginate";
 
 const Pagination = ({ pageNumber, setPageNumber }) => {
-  let next = () => {
-    setPageNumber((x) => x + 1);
-  };
-
-  let prev = () => {
-    if(pageNumber === 1) return; 
-    setPageNumber((x) => x - 1);
-  };
-  return <div className="container d-flex justify-content-center gap-5 my-5">
-    <button onClick={prev} className="btn btn-primary">Prev</button>
-    <button onClick={next} className="btn btn-primary">Next</button>
-  </div>
+  
+  return <ReactPaginate />;
 };
 
 export default Pagination
