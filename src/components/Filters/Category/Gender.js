0fa242476c34456594385c6e64975185033fe3dc
Marie Gunduz <mariegunduz@gmail.com>
2024-03-24 00:23:44 +0300

    add:created FilterBTN, modified Gender, modified Status, modified Filters

--- src/components/Filters/Category/Gender.js
+++ src/components/Filters/Category/Gender.js
@@ -4,11 +4,11 @@ const Gender = () => {
   return (
     <div className="accordion-item">
     <h2 className="accordion-header">
-      <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+      <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="true" aria-controls="collapseThree">
         Gender
       </button>
     </h2>
-    <div id="collapseOne" className="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+    <div id="collapseThree" className="accordion-collapse collapse show" data-bs-parent="#accordionExample">
       <div className="accordion-body"></div>
     </div>
   </div>
--- src/components/Filters/Category/Status.js
+++ src/components/Filters/Category/Status.js
@@ -1,15 +1,17 @@
-import React from 'react'
+import React from "react";
+import FilterBTN from "./FilterBTN";
 
 const Status = () => {
   return (
     <div className="accordion-item">
     <h2 className="accordion-header">
-      <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+      <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
         Status
       </button>
     </h2>
-    <div id="collapseThree" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
+    <div id="collapseOne" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
       <div className="accordion-body">
+        <FilterBTN/>
       </div>
     </div>
   </div>
